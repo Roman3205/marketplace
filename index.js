5a0340fa26d1a06fa54887f869bb40b7d9873d19
Roman <garbromannik@gmail.com>
2023-07-01 21:38:22 +0300

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let express = require('express')
+let app = express()
+let port = 3003
+
+app.listen(port, function() {
+    console.log('http://localhost:' + port)
+})
+
+let cors = require('cors')
+app.use(cors({origin: 'http://localhost:5173'}))
+
+app.use(express.json())
+
+let mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/sneakers-shop')
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <div class="app">
+    <div class="test">Test</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import './assets/scss/main.scss';
+</style>
